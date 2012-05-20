865aa2ffa8a5db537b84e598edb873fe4519eaf2
yann ARMAND <yann@harakys.com>
2012-05-20 18:35:53 +0200

    split Client to complete oath or simply use an existing token ( given by devise for example)

--- lib/yammer/oauth_client.rb
+++ lib/yammer/oauth_client.rb
@@ -1,45 +1,21 @@
 module Yammer
-  class Client
+  class OAuthClient < Yammer::Client
     def initialize(options={})
-      raise "Yammer::Client should not be instanciated, use Yammer::OAuthClient or Yammer::TokenClient"
-    end
+      options.assert_has_keys(:consumer, :access) unless options.has_key?(:config)
+      
+      yammer_url = options.delete(:yammer_host) || "https://www.yammer.com"
+      @api_path   = "/api/v1/"
 
-
-    # TODO: modularize message and user handling 
-    def messages(action = :all, params = {})
-      params.merge!(:resource => :messages)
-      params.merge!(:action => action) unless action == :all
-
-      parsed_response = JSON.parse(yammer_request(:get, params).body)
-      older_available = parsed_response['meta']['older_available']
-
-      ml = parsed_response['messages'].map do |m|
-         mash(m)
+      if options[:config]
+        config              = YAML.load(open(options[:config]))
+        options[:consumer]  = config['consumer'].symbolize_keys
+        options[:access]    = config['access'].symbolize_keys
       end
-        Yammer::MessageList.new(ml, older_available, self)
-    end
 
-    # POST or DELETE a message
-    def message(action, params)
-      params.merge!(:resource => :messages)
-      yammer_request(action, params)
+      consumer = OAuth::Consumer.new(options[:consumer][:key], options[:consumer][:secret], :site => yammer_url)
+      consumer.http.set_debug_output($stderr) if options[:verbose] == true
+      @access_token = OAuth::AccessToken.new(consumer, options[:access][:token], options[:access][:secret])
     end
-
-    def users(params = {})
-      params.merge!(:resource => :users)
-      JSON.parse(yammer_request(:get, params).body).map { |u| Yammer::User.new(mash(u), self) }
-    end
-
-    def user(id)
-      u = JSON.parse(yammer_request(:get, {:resource => :users, :id => id}).body)
-      Yammer::User.new(mash(u), self)
-    end
-
-    def current_user
-      u = JSON.parse(yammer_request(:get, {:resource => :users, :action => :current}).body)
-      Yammer::User.new(mash(u), self)
-    end
-    alias_method :me, :current_user
 
     private
 
---
+++ lib/yammer/token_client.rb
@@ -0,0 +1,71 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+
+module Yammer
+  class TokenClient < Yammer::Client
+    def initialize(options={})
+      options.assert_has_keys(:token)
+      
+      @yammer_url = options.delete(:yammer_host) || "www.yammer.com"
+      @api_path   = "/api/v1/"
+
+      @access_token = options[:token]
+    end
+
+    def raw_request(resource,options = {})
+      options.merge!({:resource => resource})
+      yammer_request(:get,options)
+    end
+
+    private
+
+    def yammer_request(http_method, options)
+      request_uri = @api_path + options.delete(:resource).to_s
+      [:action, :id].each {|k| request_uri += "/#{options.delete(k)}" if options.has_key?(k) }
+      request_uri += ".json"
+
+      options = { :access_token => @access_token }.merge(options)
+
+      request_uri += "?#{create_query_string(options)}" unless http_method == :post
+
+      http = Net::HTTP.new(@yammer_url, 443)
+      http.use_ssl = true
+      #  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = case http_method
+                when :post
+                  request = Net::HTTP::Post.new(request_uri)
+                  request.set_form_data(options)
+                  request
+                else
+                  Net::HTTP::Get.new(request_uri)
+                end
+      handle_response(http.request(request))
+    end
+
+    def create_query_string(options)
+      options.map {|k, v| "#{OAuth::Helper.escape(k)}=#{OAuth::Helper.escape(v)}"}.join('&')
+    end
+
+    def mash(json)
+      Mash.new(json)
+    end
+
+    def handle_response(response)
+      # TODO: Write classes for exceptions
+      case response.code.to_i
+        when 200..201
+          response
+        when 400
+          raise "400 Bad request"
+        when 401
+          raise  "Authentication failed. Check your username and password"
+        when 503
+          raise "503: Service Unavailable"
+        else
+          raise "Error. HTTP Response #{response.code}"
+        end   
+    end
+
+  end
+end
